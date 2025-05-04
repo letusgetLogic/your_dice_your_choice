7dc7de6ee3e90edb06dba5cac4d62091e9dec784
letusgetLogic <namnguyen.nn604@gmail.com>
2025-05-04 22:45:13 +0200

    Scripting - Add a dictionary for movement
    04.05.2025 - 22:44

--- Your Dice Your Choice/Assets/Scripts/Actions/Movement.cs
+++ Your Dice Your Choice/Assets/Scripts/Actions/Movement.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 
 public class Movement
 {
-    public List<string> Description;
+    public Dictionary<string, string> MoveDescription = new Dictionary<string, string>
+    {
+        {"MoveXTiles", "Move in any direction X Tiles"},
+        {"MoveX+Tiles", "Move horizontally or vertically X Tiles"},
+        {"MoveXxTiles", "Move diagonally X Tiles"},
+        {"Move1-3Tiles", "Move in any direction X Tiles, if X is in the range 1-3"},
+        {"Move1-3+Tiles", "Move horizontally or vertically X Tiles, if X is in the range 1-3"},
+        {"Move1-3xTiles", "Move diagonally X Tiles, if X is in the range 1-3"},
+        {"Move1Tile", "Move in any direction 1 Tile"},
+        {"Move1+Tiles", "Move horizontally or vertically 1 Tile"},
+        {"Move1xTiles", "Move diagonally 1 Tile"},
+    };
     
     
 }
